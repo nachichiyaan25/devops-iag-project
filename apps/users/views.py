009c8e251a677d42047737a72529bd3aa0e605aa
nachii-007 <nachikethasm25@gmail.com>
2026-04-27 11:00:18 +0530

    Initial Code SetUP

---
+++ apps/users/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from .models import User
+from .serializers import UserSerializer
+
+
+# Create your views here.
+
+#Function Based View(FBV)
+class UserCreateView(APIView):
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+#Class Based View(CBV)
+# @api_view(['POST'])
+# def create_user(request):
+#     serializer = UserSerializer(data=request.data)
+#     if serializer.is_valid():
+#         serializer.save()
+#         return Response(serializer.data)
+#     return Response(serializer.errors)
